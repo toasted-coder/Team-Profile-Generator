db4c9a04eaa38b39900d6488df85fbe711a5b81f
toasted-coder <christmanholmes21@gmail.com>
2021-03-08 16:41:33 -0800

    finished test.js for employee and engineer.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,47 @@
+
 const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+
+    describe('Initialization', () => {
+        it('should create an employee with name, id, and email', () => {
+            const employee = new Employee('John', 2, 'John@example.com');
+
+            expect(employee.name).toEqual('John');
+            expect(employee.id).toEqual(2);
+            expect(employee.email).toEqual('John@example.com');
+        });
+    })
+
+    describe('getName', () => {
+        it("should return employee's name", () => {
+            const employee = new Employee('John', 2, 'John@example.com');
+
+            expect(employee.getName()).toEqual('John');
+        })
+    })
+
+    describe('getId', () => {
+        it("should return employee's id", () => {
+            const employee = new Employee('John', 2, 'John@example.com');
+
+            expect(employee.getId()).toEqual(2);
+        })
+    })
+
+    describe('getEmail', () => {
+        it("should return employee's email", () => {
+            const employee = new Employee('John', 2, 'John@example.com');
+
+            expect(employee.getEmail()).toEqual('John@example.com');
+        })
+    })
+
+    describe('getRole', () => {
+        it("should return an employee's role", () => {
+            const employee = new Employee('John', 2, 'John@example.com');
+
+            expect(employee.getRole()).toEqual('Employee');
+        })
+    })
+})
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,55 @@
 const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+
+    describe('Initialization', () => {
+        it('should create an engineer with name, id, email, and github', () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.name).toEqual('Ricky');
+            expect(engineer.id).toEqual(3);
+            expect(engineer.email).toEqual('Ricky@example.com');
+            expect(engineer.github).toEqual('exampleEngi');
+        });
+    })
+
+    describe('getName', () => {
+        it("should return engineer's name", () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.getName()).toEqual('Ricky');
+        })
+    })
+
+    describe('getId', () => {
+        it("should return engineer's id", () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.getId()).toEqual(3);
+        })
+    })
+
+    describe('getEmail', () => {
+        it("should return engineer's email", () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.getEmail()).toEqual('Ricky@example.com');
+        })
+    })
+
+    describe('getGithub', () => {
+        it("should return engineer's github user name", () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.getGithub()).toEqual('exampleEngi');
+        })
+    })
+
+    describe('getRole', () => {
+        it("should return engineer's role", () => {
+            const engineer = new Engineer('Ricky', 3, 'Ricky@example.com', 'exampleEngi');
+
+            expect(engineer.getRole()).toEqual('Engineer');
+        })
+    })
+})
